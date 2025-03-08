cd71395a2652c78cdcc1187f805b11479438d03c
iyad <imoucharrafie@gmail.com>
2025-03-08 18:04:07 -0400

    loadCharacters

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import DetailsView from '@/views/DetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +9,13 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: HomeView,
-    }
+    },
 
+    {
+      path: '/details/:id',
+      name: 'details',
+      component: DetailsView,
+    },
   ],
 })
 
---
+++ src/views/DetailsView.vue
@@ -0,0 +1,28 @@
+<template>
+ hola
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
+  import type { Character } from '@/types/character'
+  
+  
+  const route = useRoute()
+  const id = route.params.id
+  const character = ref()
+  
+  
+  const loadCharacters = async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    const data = await response.json()
+    character.value = data
+    console.log(character.value)
+    console.log(data)
+    character.value = data
+  }
+  
+  onMounted(()=>{
+      loadCharacters()
+  })
+  </script>
